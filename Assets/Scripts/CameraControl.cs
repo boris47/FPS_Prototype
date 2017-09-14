f223ee28b6a6df34189fdc0c3debd5e8e92b84f3
RobertoLeo <boris47@hotmail.it>
2017-09-14 18:44:36 +0200

    First

---
+++ Assets/Scripts/CameraControl.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraControl : MonoBehaviour {
+
+	public static CameraControl Instance = null;
+
+	public GameObject pPlayer = null;
+
+	float fPosition_X = 0.0f;
+	float fPosition_Y = 0.0f;
+	float fPosition_Z = 0.0f;
+
+
+	float fCurrentRotation_X = 0.0f;
+	float fCurrentRotation_Y = 0.0f;
+
+
+	// Use this for initialization
+	void Start () {
+		
+		Instance = this;
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		// Position
+		transform.position = pPlayer.transform.position -  ( new Vector3 ( 0.0f, 0.0f, -10.0f ) );
+
+		fCurrentRotation_X = Mathf.Lerp( fCurrentRotation_X, Input.GetAxis ( "Mouse X" ) * 4.5f, Time.deltaTime * 50f );
+		fCurrentRotation_Y = Mathf.Lerp( fCurrentRotation_Y, Input.GetAxis ( "Mouse Y" ) * 4.5f, Time.deltaTime * 50f );
+
+		//  Horizzontal Rotation
+		transform.Rotate ( new Vector3( -fCurrentRotation_Y, 0.0f, 0.0f ), Space.Self );
+		
+		//  Vertical Rotation
+		transform.Rotate ( new Vector3( 0.0f, fCurrentRotation_X, 0.0f ), Space.World );
+
+//		PlayerController.Instance.transform.Rotate( 0.0f, fCurrentRotation_X, 0.0f );
+
+//		transform.rotation = PlayerController.Instance.transform.rotation;
+
+
+	}
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+	public static PlayerController Instance = null;
+
+	Rigidbody pRigidBody = null;
+
+	// Use this for initialization
+	void Start () {
+		
+		CameraControl.Instance.pPlayer = gameObject;
+
+		pRigidBody = GetComponent<Rigidbody>();
+
+		Instance = this;
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		float 	fMove 			= Input.GetAxis ( "Vertical" );
+		float 	fStrafe			= Input.GetAxis ( "Horizontal" );
+
+		Vector3 vTargetVelocity = new Vector3( fStrafe, 0.0f, fMove );
+
+		if ( ( fMove != 0.0f ) && ( fStrafe != 0.0f  ) ) vTargetVelocity *= 0.707f;
+
+		pRigidBody.velocity = CameraControl.Instance.transform.TransformDirection( vTargetVelocity );
+
+
+
+	}
+
+}
