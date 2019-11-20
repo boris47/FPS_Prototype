35e810b5035929ba1617511643cc5f32686834fd
boris47 <leogranderoberto@gmail.com>
2019-11-20 01:58:14 +0100

    Added Obsolete Attribute body

--- Assets/Scripts/Editor/CustomInspectors.cs
+++ Assets/Scripts/Editor/CustomInspectors.cs
@@ -45,3 +45,18 @@ public class MaterialDrawer : PropertyDrawer {
 		return 32f;
 	}
 }
+
+
+
+
+[CustomPropertyDrawer(typeof(ObsoleteInspectorAttribute))]
+class ObsoleteInspectorDrawer : PropertyDrawer
+{
+	public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+	{
+		System.ObsoleteAttribute obsoleteAttribute = ( System.ObsoleteAttribute ) System.Attribute.GetCustomAttribute(fieldInfo, typeof( System.ObsoleteAttribute ) );
+		EditorStyles.label.richText = true;
+		EditorGUI.PropertyField(position, property, new GUIContent("<color=red><i>" + label.text + "</i></color>", "**OBSOLETE**\n\n" + obsoleteAttribute.Message), true);
+		EditorStyles.label.richText = false;
+	}
+}
