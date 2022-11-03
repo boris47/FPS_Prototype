7628c08fdf4ef2f3463260a57e393ec409484a5b
Roberto Leogrande <leogranderoberto@gmail.com>
2022-11-03 15:11:10 +0100

    Fixed merge errors

--- Assets/Scripts/Game/Gameplay/Entities/AIEntity/Components/BehaviourTree/Decorators/Conditionals/BTConditional_BBBase.cs
+++ Assets/Scripts/Game/Gameplay/Entities/AIEntity/Components/BehaviourTree/Decorators/Conditionals/BTConditional_BBBase.cs
@@ -25,6 +25,7 @@ namespace Entities.AI.Components.Behaviours
 		//////////////////////////////////////////////////////////////////////////
 		protected override void StopObserve(in BTNodeInstanceData InThisNodeInstanceData)
 		{
+			if (ChildAsset.IsNotNull() && BlackboardKey.IsValid())
 			{
 				InThisNodeInstanceData.RemoveObserver(BlackboardKey, OnKeyValueChange);
 			}
@@ -34,81 +35,40 @@ namespace Entities.AI.Components.Behaviours
 		private EOnChangeDelExecutionResult OnKeyValueChange(in BlackboardInstanceData InBlackboardInstance, in BlackboardEntryKey InKey, in EBlackboardValueOp InOperation)
 		{
 			EOnChangeDelExecutionResult OutResult = EOnChangeDelExecutionResult.LEAVE;
-
-			BTNodeInstanceData thisNodeInstanceData = InBlackboardInstance.BehaviourTreeInstanceData.NodesInstanceData[NodeIndex];
-
-			if (Utils.CustomAssertions.IsTrue(m_AbortType != EAbortType.None))
 			if (ChildAsset.IsNotNull() && Utils.CustomAssertions.IsTrue(m_AbortType != EAbortType.None))
-			if (Child.IsNotNull())
 			{
-				// On value removed(set default) conditional will fail and this node will abort returning flow control to parent
-				if (InOperation == EBlackboardValueOp.REMOVE)
 				BTNodeInstanceData thisNodeInstanceData = InBlackboardInstance.BehaviourTreeInstanceData.NodesInstanceData[NodeIndex];
 				
 				// On value removed(set default) conditional will fail and this node will abort returning flow control to parent
 				if (InOperation == EBlackboardValueOp.REMOVE)
-				if (Utils.CustomAssertions.IsTrue(m_AbortType != EAbortType.None))
 				{
-					BTNodeInstanceData thisNodeInstanceData = InBlackboardInstance.BehaviourTreeInstanceData.NodesInstanceData[NodeIndex];
+					OutResult = AbortSelf(thisNodeInstanceData);
+				}
 
-					// On value removed(set default) conditional will fail and this node will abort returning flow control to parent
-					if (InOperation == EBlackboardValueOp.REMOVE)
+				// On value changed (from value to value (not default one)), if guard is set, abort running child, reset and restart it
+				else if (InOperation == EBlackboardValueOp.CHANGE)
+				{
+					if (m_ResetOnChange)
 					{
 						if (Utils.CustomAssertions.IsTrue(thisNodeInstanceData.NodeState == EBTNodeState.RUNNING))
 						{
-							BTNodeInstanceData childInstanceData = GetChildInstanceData(thisNodeInstanceData, Child);
-							Child.AbortAndResetNode(childInstanceData);
 							BTNodeInstanceData childInstanceData = GetNodeInstanceData(thisNodeInstanceData, ChildAsset);
 							ChildAsset.AbortAndResetNode(GetNodeInstanceData(thisNodeInstanceData, ChildAsset));
-							OutResult = AbortSelf(thisNodeInstanceData);
-						}
-					}
 
-					// On value changed (from value to value (not default one)), if guard is set, abort running child, reset and restart it
-					else if (InOperation == EBlackboardValueOp.CHANGE)
-					{
-						// If this node is running then process the change event
-						if (thisNodeInstanceData.NodeState == EBTNodeState.RUNNING)
-						{
-							if (m_ResetOnChange)
-							{
-								// Abort and reset child
-								BTNodeInstanceData childInstanceData = GetChildInstanceData(thisNodeInstanceData, Child);
-								Child.AbortAndResetNode(childInstanceData);
-
-								// Set this node ar running one to update child
-								thisNodeInstanceData.BehaviourTreeInstanceData.SetRunningNode(thisNodeInstanceData);
-							}
-						}
-						// Otherwise this could mean that the node has failed previously and now observer is live so we want to remove it
-						else
-						{
-							OutResult = EOnChangeDelExecutionResult.REMOVE;
-						}
-					}
-
-							childInstanceData.BehaviourTreeInstanceData.SetRunningNode(childInstanceData);
 							childInstanceData.SetAsRunningNode();
-					// On value added (from default to a valid value) request the tree to abort running branch and move to this node and run it
-					else
-					{
-						// We expect this node not to be in running state
-						if (Utils.CustomAssertions.IsTrue(thisNodeInstanceData.NodeState != EBTNodeState.RUNNING))
-						{
-							RequestLowPriorityAbort(thisNodeInstanceData);
 						}
 					}
 				}
-				// No point to keep observing if abort type is None
+
+				// On value added (from default to a valid value) request the tree to abort running branch and move to this node and run it
 				else
 				{
-					OutResult = EOnChangeDelExecutionResult.REMOVE;
+					// We expect this node not to be in running state
+					if (Utils.CustomAssertions.IsTrue(thisNodeInstanceData.NodeState != EBTNodeState.RUNNING))
+					{
+						RequestLowPriorityAbort(thisNodeInstanceData);
+					}
 				}
-			}
-			// At this point there should be a child but whenever this condition is not met, we remove the observer
-			else
-			{
-				OutResult = EOnChangeDelExecutionResult.REMOVE;
 			}
 			return OutResult;
 		}
